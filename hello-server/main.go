b0733d47cce93696a081efdc67928c68983c94aa
Syncorsand <80617088+Syncorsand@users.noreply.github.com>
2022-10-21 00:05:39 +0900

    コメント消去

--- hello-server/main.go
+++ hello-server/main.go
@@ -7,21 +7,11 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type jsonData struct {
-	Number int
-	String string
-	Bool   bool
-}
-
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// e.GET("/hello", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello,World\n")
-	// })
 
 	e.POST("/hello/:name", helloHandler)
 
@@ -34,26 +24,3 @@ func helloHandler(c echo.Context) error {
 	name := c.Param("name")
 	return c.String(http.StatusOK, "Hello, "+name+".\n")
 }
-
-// func jsonHandler(c echo.Context) error {
-// 	res := jsonData{
-// 		Number: 10,
-// 		String: "hoge",
-// 		Bool:   false,
-// 	}
-
-// 	return c.JSON(http.StatusOK, &res)
-// }
-
-// func postHandler(c echo.Context) error {
-// 	data := jsonData{
-// 		Number: 30,
-// 		String: "huge",
-// 		Bool:   true,
-// 	}
-
-// 	if err := c.Bind(&data); err != nil {
-// 		return c.JSON(http.StatusBadRequest, data)
-// 	}
-// 	return c.JSON(http.StatusOK, data)
-// }
