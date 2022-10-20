d85670fe9eb0ecec6e548b459302a21e40e268ca
Syncorsand <80617088+Syncorsand@users.noreply.github.com>
2022-10-20 23:40:50 +0900

    goでのサーバー、/hello,/json

---
+++ hello-server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type jsonData struct {
+	Number int
+	String string
+	Bool   bool
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello,World\n")
+	})
+
+	e.GET("/json", jsonHandler)
+
+	e.Start(":8080")
+}
+
+func jsonHandler(c echo.Context) error {
+	res := jsonData{
+		Number: 10,
+		String: "hoge",
+		Bool:   false,
+	}
+
+	return c.JSON(http.StatusOK, &res)
+}
